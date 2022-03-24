55ef9124d0458ca9d5ea0e9af44a78fbd02f209f
sid7631 <sidsarkar07@gmail.com>
2022-03-24 21:49:12 +0530

    added react client

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template,send_from_directory,request, jsonify, make_response
+from flask_cors import CORS, cross_origin
+import os
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+app = Flask(__name__ ,static_folder='client/build',static_url_path='')
+cors = CORS(app)
+
+
+@app.route('/api')
+@cross_origin()
+def Welcome():
+    return "Welcome to the API!!!"
+
+@app.route('/')
+def serve():
+    return send_from_directory(app.static_folder, 'index.html')
+
 if __name__ == "__main__":
-   app.run(host='0.0.0.0:8000',debug=True)
+   app.run(debug=True)
